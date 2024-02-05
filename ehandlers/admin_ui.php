9f3341a0844575818322cbd5384473ef112d7890
camer0n <e107inc@gmail.com>
2024-02-05 13:59:52 -0800

    Fixes #5177 Deletion issue in grid mode.

--- ehandlers/admin_ui.php
+++ ehandlers/admin_ui.php
@@ -6202,6 +6202,12 @@ class e_admin_ui extends e_admin_controller_ui
 		return $cnt;
 	}
 
+	public function GridDeleteTrigger($posted)
+	{
+		$this->ListDeleteTrigger($posted);
+	}
+
+
 	/**
 	 * Catch delete submit
 	 * @param $posted
